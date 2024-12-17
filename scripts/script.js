c2f7484dbf583d3934fe039b6d9c454154d7da72
Lizaveta Maslova <elizaveta-maslova-2000@mail.ru>
2024-12-17 18:54:25 +0300

    сверстала 1 блок

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// let redColor = (document.querySelector(".main-title").style.color = "red");
+//  let image = document.querySelector(".image-cook");
+//  image.setAttribute("src", "./images/cookie.png");
+// console.log(image.hasAttribute("src"));
+const bodyElement = document.body;
+
+const bodyChildren = bodyElement.children;
+console.log(bodyChildren);
